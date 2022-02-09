ef070803d13b8787c69148fcb95c00cfff7f9aaa
sershishkov <ser.shishkov>
2022-02-09 08:07:27 +0200

    100

---
+++ 003_relations/02-create-tables.sql
@@ -0,0 +1,22 @@
+CREATE TABLE users (
+  -- id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+  id SERIAL PRIMARY KEY , -- Posgresql
+  first_name VARCHAR(300) NOT NULL,
+  last_name VARCHAR(300) NOT NULL,
+  email VARCHAR(300) NOT NULL,
+  address_id INT NOT NULL
+);
+
+CREATE TABLE addresses (
+  -- id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+  id SERIAL PRIMARY KEY , -- Posgresql
+  street VARCHAR(300) NOT NULL,
+  house_number VARCHAR(50) NOT NULL,
+  city_id INT NOT NULL
+);
+
+CREATE TABLE cities (
+  -- id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+  id SERIAL PRIMARY KEY , -- Posgresql
+  name VARCHAR(300) NOT NULL  
+);
