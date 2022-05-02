3ff79f3144791dee6a320bc795a38e8a28a4013c
sershishkov <ser.shishkov@gmail.com>
2022-05-02 11:40:39 +0300

    148

---
+++ 006_aggrFuncGrouping/useDataBase/07-nested-subqueries.sql
@@ -0,0 +1,13 @@
+SELECT booking_date
+FROM bookings
+GROUP BY booking_date
+HAVING SUM(amount_billed) = ( 
+  SELECT MIN(daily_sum)
+    FROM (
+      SELECT booking_date, SUM(amount_billed) AS daily_sum
+      FROM bookings
+      GROUP BY booking_date
+    ) AS dayly_table
+ )
+  
+;
