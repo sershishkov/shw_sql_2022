cc27e23e35cdb9ee93b36aabbb9dce9004f4fdb4
sershishkov <ser.shishkov@gmail.com>
2022-05-06 08:47:38 +0300

    209

--- 010-projectMyEvents/005-create-locations-table.sql
+++ 010-projectMyEvents/005-create-locations-table.sql
@@ -10,7 +10,7 @@ CREATE TABLE locations (
    street VARCHAR(300) NOT NULL,
    house_number VARCHAR(300) NOT NULL,
    postal_code VARCHAR(300) NOT NULL
-   city_name VARCHAR(200) REFERENCES cities
+   city_name VARCHAR(200) REFERENCES cities ON DELETE RESTRICT ON UPDATE CASCADE
  )
 ;
 
@@ -23,5 +23,5 @@ CREATE TABLE events (
   description TEXT NOT NULL,
   max_participants INT CHECK (max_participants > 0),
   min_age INT CHECK (min_age > 0),
-  location_id INT REFERENCES locations
+  location_id INT REFERENCES locations ON DELETE CASCADE 
  );
