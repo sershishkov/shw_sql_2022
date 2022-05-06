27aa24753f82b5a7c5a527cda6b9b3f329d35ab0
sershishkov <ser.shishkov@gmail.com>
2022-05-06 09:48:17 +0300

    212

--- 010-projectMyEvents/009-queries-with-relations.sql
+++ 010-projectMyEvents/009-queries-with-relations.sql
@@ -2,6 +2,18 @@
 -- INNER JOIN locations AS loc ON e.location_id = loc.id
 -- ;
 
+-- SELECT 
+-- e.id AS event_id,
+-- e.name,
+-- e.date_planned,
+-- loc.title,
+-- loc.street,
+-- loc.house_number,
+-- loc.city_name
+-- FROM events AS e
+-- INNER JOIN locations AS loc ON e.location_id = loc.id
+-- ;
+
 SELECT 
 e.id AS event_id,
 e.name,
@@ -9,7 +21,11 @@ e.date_planned,
 loc.title,
 loc.street,
 loc.house_number,
-loc.city_name
+loc.city_name,
+u.first_name,
+u.last_name
 FROM events AS e
 INNER JOIN locations AS loc ON e.location_id = loc.id
+INNER JOIN events_users AS eu ON eu.event_id = e.id
+INNER JOIN users AS u ON eu.user_id = u.id
 ;
